fdc7dfe32e8f008950153c11d4e43e1bb0c9e8aa
Rohit Batra <rohit@rohitbatra.in>
2016-12-31 13:26:53 +0530

    changed layout of footer & categories listing in footer

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -5,12 +5,9 @@
 		<footer>
 			<div class="row">
 				<div class="col-md-3">
-					<img src="{{ asset('images/logo/logo_200x65.png') }}" />
-				</div>
-				<div class="col-md-3">
 					<h4>About Us</h4>
 					@if(isset($navigations['about']))
-						<ul>
+						<ul class="ul-noBullets col-sm-4-8 column-0">
 							@foreach($navigations['about'] as $n)
 								<li>
 									<a href="{{{$n['url']}}}" title="{{{$n['title']}}}">
@@ -24,7 +21,7 @@
 				<div class="col-md-3">
 					<h4>Help</h4>
 					@if(isset($navigations['help']))
-						<ul>
+						<ul class="ul-noBullets">
 							@foreach($navigations['help'] as $n)
 								<li>
 									<a href="{{{$n['url']}}}" title="{{{$n['title']}}}">
@@ -36,29 +33,85 @@
 					@endif
 				</div>
 				<div class="col-md-3">
-					<h4>Category</h4>
+					<h4>Discover</h4>
 					@if(isset($navigations['categories']))
-						<ul>
-							@foreach($navigations['categories'] as $n)
-								<li>
-									<a href="{{{$n['url']}}}" title="{{{$n['title']}}}">
+						<?php
+							$cnt = count($navigations['categories']);
+
+							if($cnt > 5)
+							{
+								$cntr = 0;
+								$str = "<ul class='pull-left ul-noBullets'>";
+									foreach ($navigations['categories'] as $n)
+									{
+										if((($cntr % 5) == 0) && $cntr !== 0)
+										{
+											$str .= "</ul>
+														<ul class='pull-right ul-noBullets'>
+															<li>
+																<a href='".$n['url']."' title='".$n['title']."'>
+																{$n['title']}
+															</li>
+														";
+										} else {
+											$str .= "<li>
+														<a href='".$n['url']."' title='".$n['title']."'>
+														{$n['title']}
+													 </li>";
+										}
+										$cntr++;
+									}
+								$str .="</ul>";
+								print($str);
+							} else { ?>
+							<ul class="ul-noBullets">
+								@foreach($navigations['categories'] as $n)
+									<li>
+										<a href="{{{$n['url']}}}" title="{{{$n['title']}}}">
 										{{{$n['title']}}}
-									</a>
-								</li>
-							@endforeach
-						</ul>
+										</a>
+									</li>
+								@endforeach
+							</ul>
+							<?php
+							}
+						?>
 					@endif
 				</div>
 			</div>
-			<hr>
+			<hr class="tiny">
 			<div class="row">
-				<div class="col-md-12 text-center">
-				All Rights Reserved  &copy; 2015 - {{{date('Y')}}}
-				@yield('footer')
+				<div class="col-md-3">
+					<img src="{{ asset('images/logo/logo_200x65.png') }}" class="footer_logo" />
 				</div>
-				<div class="col-md-6 text-right">
-					<ul>
-					</ul>
+
+				<div class="col-md-3 pull-right">
+
+					<a target="_blank" href="http://www.facebook.com/PoloniaGo">
+						<img src="{{{ asset('images/social_icons/FB_logo_29.png') }}}" class="social_icons_homepage" />
+					</a>
+
+					<a target="_blank" href="http://www.twitter.com/poloniago">
+						<img src="{{{ asset('images/social_icons/TW_logo.png') }}}" class="social_icons_homepage" />
+					</a>
+
+					<a target="_blank" href="https://www.youtube.com/user/PoloniaGo">
+						<img src="{{{ asset('images/social_icons/YT_logo_24.png') }}}" class="social_icons_homepage" />
+					</a>
+
+					<a target="_blank" href="https://www.instagram.com/poloniago/">
+						<img src="{{{ asset('images/social_icons/IG_logo.png') }}}" class="social_icons_homepage" />
+					</a>
+
+					<a target="_blank" href="#" >
+						<img src="{{{ asset('images/social_icons/IN_logo_28.png') }}}" class="social_icons_homepage" />
+					</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-3">
+					&nbsp;&nbsp; All Rights Reserved  &copy; {{{date('Y')}}}
+					@yield('footer')
 				</div>
 			</div>
 		</footer>
