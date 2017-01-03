49d7152492417015875d1ef7aff8ed7d71883483
Rohit Batra <rohit@rohitbatra.in>
2017-01-03 18:32:59 +0530

    register email changes

---
+++ resources/views/email/footer.blade.php
@@ -0,0 +1,17 @@
+<div class="footer">
+    <table border="0" cellpadding="0" cellspacing="0">
+        <tr>
+            <td class="content-block">
+                <a href="https://poloniago.com"><img src="{{{asset('images/logo/logo_105x21.png')}}}" /></a>
+                <span class="apple-link">Company Inc, 3 Abbey Road, San Francisco CA 94102</span> <br>
+            </td>
+        </tr>
+    </table>
+</div>
+</div>
+</td>
+<td>&nbsp;</td>
+</tr>
+</table>
+</body>
+</html>
--- resources/views/email/thankyou.blade.php
+++ resources/views/email/thankyou.blade.php
@@ -1,20 +1,35 @@
 @extends('email.general')
 @section('emailbody')
 
-<h3>Hello {{{$user->fullname}}}</h3>
-<p>
-Thank You for Registering.
-</p>
-<p>
-If you've not registered with us at <a href="https://www.poloniago.com">PoloniaGo</a>, you can just ignore this email.
-</p>
-<p>
-This is an automated response, please do not reply to this email. If you want to contact us, mail us at <a href="mailto:support@poloniago.com">support@poloniago.com</a> .
-</p>
+<table border="0" cellpadding="0" cellspacing="0" class="body">
+    <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+            <div class="content"><span class="preheader">Thank you for signing-up with PoloniaGo.</span>
+                <table class="main">
+                    <tr>
+                        <td class="wrapper">
+                            <table border="0" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td>
+                                        <p>Hi {{{$user->fullname}}},</p>
+                                        <p>Thank You for Registering.</p>
+                                        <p>If you've not registered with us at
+                                            <a href="https://poloniago.com">PoloniaGo</a>, you can just ignore this
+                                            email.
+                                        </p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>
+                                        <p>This is an automated response, please do not reply to this email. If you want
+                                            to contact us, mail us at <a href="mailto:support@poloniago.com">support@poloniago.com</a></p>
 
-<p>
-Regards,
-PoloniGo Team
-
-</p>
+                                        <p>Thanks!</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
 @stop
