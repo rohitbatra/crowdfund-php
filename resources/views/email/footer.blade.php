082ca8f6183de5f6a225b611642a0ad96ad29186
Rohit Batra <rohit@rohitbatra.in>
2017-01-03 18:51:54 +0530

    changed logo size & placement

--- resources/views/email/footer.blade.php
+++ resources/views/email/footer.blade.php
@@ -2,7 +2,7 @@
     <table border="0" cellpadding="0" cellspacing="0">
         <tr>
             <td class="content-block">
-                <a href="https://poloniago.com"><img src="{{{asset('images/logo/logo_105x21.png')}}}" /></a>
+                <a href="https://poloniago.com"><img src="{{{asset('images/logo/logo_200x65.png')}}}" /></a><br/>
                 <span class="apple-link">Company Inc, 3 Abbey Road, San Francisco CA 94102</span> <br>
             </td>
         </tr>
